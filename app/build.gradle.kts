2a8e9682c9d9704fc78de8e69bd704aa4a441038
AlexhpSENATI <1510696@senati.pe>
2025-02-28 10:24:30 -0500

    cambio 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.proyectoviernes2"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.proyectoviernes2"
